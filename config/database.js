4f340c51a6f9b3ead8939fe88f4ff254e5418714
asvpxmani <eroppong@gmail.com>
2018-07-07 16:15:18 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // configure the code below with your username, password and mlab database information
+  database: 'mongodb://kwasidb:kwasidb23@ds125881.mlab.com:25881/geecuts1',   //prod
+  //database: 'mongodb://localhost:27017/meanauth',    //dev
+  secret: 'yoursecret'
+}
